c869996aa10a2dd3fb1477bb011f8465fd540e38
ShahbajKhan <shktoky@gmail.com>
2023-07-17 22:14:27 +0600

    basic db and env setup done

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+const helmet = require("helmet");
+
+const AppError = require("./utils/appError");
+// const { verifyToken } = require("./utils/verifyToken");
+// const config = require("./config");
+// const globalErrorHandler = require("./middlewares/globalErrorHandler");
+
+// set security http headers
+app.use(helmet());
+
+
+// $ CORS
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/api/v1", (req, res) => res.send("Welcome to P.H. Forum!"));
+
+// handling all (get,post,update,delete.....) unhandled routes
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
+});
+
+
+
+module.exports = app;
---
+++ utils/dbConnect.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const { NODE_ENV, DATABASE_PROD,DATABASE_LOCAL } = process.env;
+
+let DB_URL;
+
+// if (process.argv[2] && process.argv[2] === "dblocal") DB_URL = DBLocal;
+if (NODE_ENV === "production") {
+  DB_URL = DATABASE_PROD;
+} else if (NODE_ENV === "development") {
+  DB_URL = DATABASE_LOCAL;
+}
+
+module.exports = () => {
+  console.log(`connecting to ${NODE_ENV} DB...`);
+  mongoose
+    .connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB connection successful!"))
+    .catch((err) => {
+      console.log("DB Connection Failed !");
+      console.log("err", err);
+    });
+};
